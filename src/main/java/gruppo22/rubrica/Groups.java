52b5b78fb4d73f5cc1f89960991604a497b1631c
loreal24 <loriagiuseppepio1@gmail.com>
2024-12-08 16:40:38 +0100

    Added comments to Groups

--- src/main/java/gruppo22/rubrica/Groups.java
+++ src/main/java/gruppo22/rubrica/Groups.java
@@ -1,30 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file Groups.java
+ * @brief This file contains the implementation of the List of Groups created by
+ * the user.
+ * @author loreal
  */
 package gruppo22.rubrica;
 
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- *
- * @author loreal
- */
 public class Groups {
 	private List<Group> groups;
 
+	/**
+	 * @brief Default Constructor
+	 */
 	public Groups() {
 		groups = new LinkedList<>();
 	}
 
+	/**
+	 * @brief Allows to add a group to the list of groups 
+	 * @param g The group you want to add
+	 */
 	public void addGroup(Group g) {
 		groups.add(g);	
 
 		groups.sort(new GroupComparator());
 	}
 
+	/**
+	 * @brief Allows to remove a group from the list of groups
+	 * @param g The group you want to remove
+	 */
 	public void removeGroup(Group g) {
 		if(!groups.isEmpty()) {
 			groups.remove(g);
