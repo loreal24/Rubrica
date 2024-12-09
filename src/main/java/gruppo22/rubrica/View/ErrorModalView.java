7af7c6934a9d98d581946c752ad1a8130154397b
simon <simon@172.19.233.173>
2024-12-09 19:26:34 +0100

    ErrorModal

---
+++ src/main/java/gruppo22/rubrica/View/ErrorModalView.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gruppo22.rubrica.View;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author simon
+ */
+public class ErrorModalView extends Pane{
+    
+    public ErrorModalView(){
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/src/main/resources/gruppo22/rubrica/errorModal.fxml"));
+
+		loader.setController(getClass().getResource("ErrorModalController.java"));
+		try{
+			Parent root = loader.load();
+			getChildren().add(root);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}	
+}
